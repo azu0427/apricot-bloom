db0e21a63c49c5759edc386f055b71e65f314834
azu0427 <azu.kook.0427@gmail.com>
2025-03-08 18:18:50 +0900

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(".openbtn1").click(function () {
-  $(this).toggleClass('active')
-  $('.nav-sp').toggleClass('open')
-})
-
-$(".nav-sp a").click(function () {
-  $(this).parent().parent().parent().parent().toggleClass('open')
-  $(".openbtn1").toggleClass('active')
-})
